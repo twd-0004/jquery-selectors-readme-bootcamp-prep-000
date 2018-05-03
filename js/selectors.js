fad45ed41a9076ed43856e1a965245603fbfb618
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 16:19:54 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,3 +5,12 @@ function paragraphSelector() {
 function lastImageSelector() {
   return $('img:last')
 }
+function ninjaBabySelector() {
+  return $('#baby-ninja')
+}
+function divSelector() {
+  return $('.pics')
+}
+function firstListItem() {
+  return $('#pic-list li:first-child')
+}
